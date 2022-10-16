4a37c76e348fc9791e3daefd9d363f190e578a94
brittneypostma <bdesignwebdev@gmail.com>
2022-10-15 23:45:28 -0400

    pnpm

--- src/routes/custom/+page.server.js
+++
@@ -1,3 +0,0 @@
-export const load = (req, context) => {
-  console.log(context)
-}
--- svelte.config.js
+++ svelte.config.js
@@ -1,10 +1,12 @@
-import adapter from '@sveltejs/adapter-auto';
+import adapter from '@sveltejs/adapter-netlify'
 
 /** @type {import('@sveltejs/kit').Config} */
 const config = {
-	kit: {
-		adapter: adapter()
-	}
-};
+  kit: {
+    adapter: adapter({
+      edge: true
+    })
+  }
+}
 
-export default config;
+export default config
